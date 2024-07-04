eb43e4e6c46bc4b57df0883814f783b4e53dbbc3
Koushik Mondal <darkdeavil004@gmail.com>
2024-07-04 12:12:43 +0530

    navigation added

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
-import { View, Text, StyleSheet } from 'react-native';
+// In App.js in a new project
 
-// create a component
-const MyComponent = () => {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <View>
-      <Text>MyComponent</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
 
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
-  },
-});
+const Stack = createNativeStackNavigator();
 
-//make this component available to the app
-export default MyComponent;
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
