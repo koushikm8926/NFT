4a11d40a68c61ad70438534589dfea017f99fc25
Koushik Mondal <darkdeavil004@gmail.com>
2024-07-04 11:44:24 +0530

    lets start with the development

---
+++ App.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const MyComponent = () => {
+  return (
+    <View>
+      <Text>MyComponent</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default MyComponent;
